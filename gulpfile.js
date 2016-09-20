cec848d52840753600dabb8404a23a8bfb8d62b7
Joe Dallacqua <joseph.m.dallacqua@gmail.com>
2016-09-20 14:54:36 -0600

    concatenate setup

--- gulpfile.js
+++ gulpfile.js
@@ -1,6 +1,15 @@
-var gulp = require ('gulp')
-	gutil = require('gulp-util')
+var gulp = require ('gulp'),
+	gutil = require('gulp-util'),
+	concat = require('gulp-concat');
 
+var jsSources =['components/scripts/test.js']	
+	
 gulp.task('log',function(){
 	gutil.log('workflows are awesome');
+});
+
+gulp.task('js',function(){
+	gulp.src(jsSources)
+		.pipe(concat('script.js'))
+		.pipe(gulp.dest('builds/development/js'));
 });
